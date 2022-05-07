f578eedf19201c78487f4a2a3e7196db558a0183
Reyhaneh Savari <reyhaneh.sa.mo@gmail.com>
2022-05-07 22:46:21 +0430

    add code sidebar component

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,12 +1,38 @@
-import React from 'react';
-import './sidebar.css';
+import React from "react";
+import imgSidebar from "../../asserts/imgages/imgSidebar.jpg";
+import facebook from '../../asserts/imgages/facebook.png';
+import twitter from '../../asserts/imgages/twitter.png';
+import pinterest from '../../asserts/imgages/pinterest.png';
+import instagram from '../../asserts/imgages/instagram.png';
+import "./sidebar.css";
 
 const Sidebar = () => {
-    return (
-        <div className="sidebarContainer">
-            ihnkm'
-        </div>
-    );
+  return (
+    <div className="sidebarContainer">
+      <p className="sidebarTitle">ABOUT US</p>
+      <img src={imgSidebar} className="sidebarImg" alt="image" />
+      <p className="sidebarText">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.
+      </p>
+      <p className="sidebarTitle">CATEGORIES</p>
+      <div className="sidebarCategories">
+        <span className="sidebarCategory">Life</span>
+        <span className="sidebarCategory">Music</span>
+        <span className="sidebarCategory">Style</span>
+        <span className="sidebarCategory">Sport</span>
+        <span className="sidebarCategory">Tech</span>
+        <span className="sidebarCategory">Cinema</span>
+      </div>
+      <p className="sidebarTitle">FOLLOW US</p>
+      <div className="sidebarIcons">
+        <img src={facebook} className="sidebarIcon" alt="images" /> 
+        <img src={twitter} className="sidebarIcon" alt="images" /> 
+        <img src={pinterest} className="sidebarIcon" alt="images" /> 
+        <img src={instagram} className="sidebarIcon" alt="images" /> 
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
